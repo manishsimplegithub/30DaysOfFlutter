196e60ab93d06ef8db18fff09779e48b73edcc9b
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-07-11 17:07:45 +0530

    day 3 completed

--- Flutter_New/flutter_application_1/lib/main.dart
+++ Flutter_New/flutter_application_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(myApp());
@@ -7,11 +8,20 @@ void main() {
 
 class myApp extends StatelessWidget {
   const myApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      darkTheme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      initialRoute: '/home',
+      routes: {
+        "/": (context) => LoginPage(),
+        "/Home": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
